18e100a7ffd816f97f9c6967026367ba891eb2ee
Juan Carlos López Pimentel <jclopezpimentel@gmail.com>
2019-11-14 19:23:34 -0600

    Contract created and visualized with ajax
    In this version we have created a contract. The address of the contract also can be displayed.

---
+++ appAvocadoWeb/public/javascripts/reqContractAddress.js
@@ -0,0 +1,12 @@
+document.getElementById("body").onload = function() {reqContractAdd()};
+
+function reqContractAdd() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      	document.getElementById("divAnswer").innerHTML = xhttp.responseText;
+    }
+  };
+  xhttp.open("GET", "/getMyContract", true);
+  xhttp.send();
+}
