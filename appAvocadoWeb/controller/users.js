4b1f3c9b59393cd65bf38b62651b1e7cb24d2e9a
Juan Carlos López Pimentel <jclopezpimentel@gmail.com>
2019-11-19 17:32:03 -0600

    login incompleto
    Incompleto en mostrar autentificación del servidor

--- appAvocadoWeb/controller/users.js
+++ appAvocadoWeb/controller/users.js
@@ -1,13 +1,35 @@
 var mongoose = require('mongoose');
-//var Root = require("../models/Root");
+var Root = require("../models/Root");
 
 var initializer = {};
 
 
 initializer.login = function(req, res) {
-	pass = req.body.password; //obtaining public key account
-    res.send("password:"+ pass);
-    res.end();
+	email = req.body.email;
+	var word = "root@root.jc"; 
+	if(email===word){
+		r = loguear(res,word);
+		if(r===false){
+			res.render('gral',{output:'Usuario colocó password incorrecto'});
+		}
+	}else{
+		res.render('gral',{output:'Validar autentificación de usuario'});
+	}
+}
+
+
+function loguear(res,word){
+	Root.find({email:word}).exec(function(err, users){
+        if( err ){ 
+        	console.log('Error: ', err); 
+        	return false; 
+        }
+        //res.send(users.password);
+        res.render('gral',{ output:users.password });
+        return true;
+        //res.render('../views/product/index', {products: products} );
+    });
+    return true;
 }
 
 
